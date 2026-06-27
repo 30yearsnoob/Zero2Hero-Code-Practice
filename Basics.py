b9739eddc4cd12fa3bd2c63d61148e2e290d95d9
DESKTOP-2JR7I6B\seb <nintendo.ebioja@gmail.com>
2026-06-27 10:05:51 +0800

    Practice Python 3  Chapter 1-4 06/27/26

---
+++ Basics.py
@@ -0,0 +1,39 @@
+#Exercises
+#1 Use Print
+#print("Hello World")
+
+#2 Create Variable
+# greeting = "Hello World"
+# print(greeting)
+
+#3 Concatenation
+# print(
+#     """She said \"hey how d\'you do? \
+#     I\'m fine, thank you"""
+#     )
+
+# print(
+#     """She said \"hey how d\'you do? 
+#     I\'m fine, thank you"""
+#     )
+
+#4 Slice
+# word = "sample" + " " +"exercises"
+# print(len(word))
+# print(word[6:10])
+
+#5  string methods to alter each string so that .startswith("be") 
+# returns True for all of them.
+# string1 = "Becomes" 
+# string2 = "becomes" 
+# string3 = "BEAR" 
+# string4 = " bEautiful"
+
+# print(string2.lower().startswith("be")) 
+# print(string1.lower().startswith("be")) 
+# print(string3.lower().startswith("be")) 
+# print(string4.lower().lstrip().startswith("be")) 
+
+#6
+# userInput = input("Write something: ")
+# print(len(userInput))
